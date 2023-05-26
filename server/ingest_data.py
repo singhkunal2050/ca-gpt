962b1d58bf68eb667f9791ddb8c493c46938e641
Replit user <>
2023-05-26 17:20:23 +0000

    test

---
+++ server/ingest_data.py
@@ -0,0 +1,37 @@
+
+from langchain.document_loaders import WebBaseLoader
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+
+import pickle
+import os
+
+def create_vector_store():
+
+    if os.path.exists('vectorstore.pkl'):
+        print("Store Already exists")
+        return FAISS.load_local('vectorstore.pkl', HuggingFaceEmbeddings)
+
+    print("Ingesting the required doc .. ")
+
+    loader = WebBaseLoader("https://cleartax.in/s/house-property")
+    raw_documents = loader.load()
+    #loader = ReadTheDocsLoader("https://cleartax.in/s/house-property")
+    # raw_documents = loader.load()
+    print("Docs loaded", raw_documents)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=600,
+        chunk_overlap=150,
+    )
+    documents = text_splitter.split_documents(raw_documents)
+    print("Documents successfully chunked")
+    embeddings = HuggingFaceEmbeddings()
+
+    print(documents)
+    vectorstore = FAISS.from_documents(documents, embeddings)
+
+    with open("vectorstore.pkl", "wb") as f:
+        pickle.dump(vectorstore, f)
+
+    return vectorstore
--- server/main.py
+++ server/main.py
@@ -1,7 +1,56 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+import logging
+from pathlib import Path
+import pickle
+from typing import Optional
+
+
+from langchain.llms import OpenAI
+from langchain import ConversationChain
+from langchain.chains import LLMChain
+from langchain.vectorstores import VectorStore
+
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+
+from ingest_data import create_vector_store
+from schemas.request_schemas import RequestBodyParams
+
 
 app = FastAPI()
 
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+llm = OpenAI(openai_api_key="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", temperature=0.5, max_tokens=1000, frequency_penalty=0.2)
+vectorstore: Optional[VectorStore] = None
+
+@app.on_event("startup")
+async def startup_event():
+    logging.info("loading vectorstore")
+    if not Path("vectorstore.pkl").exists():
+        create_vector_store()
+        raise ValueError("vectorstore.pkl does not exist, please run ingest.py first")
+    with open("vectorstore.pkl", "rb") as f:
+        global vectorstore
+        vectorstore = pickle.load(f)
+        print("Vector store loaded successfully")
+
 @app.get("/")
-def root():
-    return {"message": "Hello, World!"}
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/api/v1/completion")
+async def get_completion(params: RequestBodyParams):
+
+    print("User Query", params.user_query)
+    print("meta", params.meta)
+
+    conversationQA = ConversationalRetrievalChain.from_llm(llm, vectorstore.as_retriever(), memory=memory)
+
+    response = conversationQA({"question": params.user_query})
+
+    print(response)
+
+    return response["answer"]
